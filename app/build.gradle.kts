09a4931a4e556b5ae09e6d32b8643b0138fcdc72
thibautfihey49-hue <thibautfihey49-hue@users.noreply.github.com>
2026-09-02 13:02:38 +0200

    ✅ Fix: icons-extended au lieu de icons-full

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,5 +32,5 @@ dependencies {
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.material:material-icons-core")
-    implementation("androidx.compose.material:material-icons-full")
+    implementation("androidx.compose.material:material-icons-extended")
 }
