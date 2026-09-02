e84e0fca9da8c54746c51929070ae431e36cea5d
thibautfihey49-hue <thibautfihey49-hue@users.noreply.github.com>
2026-09-02 14:14:38 +0200

    ✅ Fix: versions Compose/Kotlin compatibles

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,11 +26,13 @@ android {
     }
     kotlinOptions {
         jvmTarget = "17"
-    suppressKotlinVersionCompatibilityCheck = true
         freeCompilerArgs += "-opt-in=androidx.compose.material3.ExperimentalMaterial3Api"
     }
     buildFeatures {
         compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.3"
     }
 }
 
