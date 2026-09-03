95015de2b6383c9beaf7fdca3753c6d397463b78
thibautfihey49-hue <thibautfihey49-hue@users.noreply.github.com>
2026-09-03 16:50:44 +0200

    🔧 Fix: Ajout import Bundle manquant

--- app/src/main/java/com/bloom/gps/LocationTrackerService.kt
+++ app/src/main/java/com/bloom/gps/LocationTrackerService.kt
@@ -10,6 +10,7 @@ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import android.os.Build
+import android.os.Bundle
 import android.os.IBinder
 import android.telephony.SmsManager
 import android.util.Log
